bb2ecfa3c62be0a4adf2177eae07c1abc95e94a8
SoyElMatra <matramatrayt@gmail.com>
2025-03-21 17:19:33 -0300

    Add files via upload

---
+++ Clases/Clases/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona();
+            Persona p2 = new Persona();
+            p1.AsignarEdad(20);
+            p2.AsignarEdad(18);
+            Console.Write("Edad 1: ");
+            p1.MostrarEdad();
+            Console.Write("Edad 2: ");
+            p2.MostrarEdad();
+        }
+    }
+}
