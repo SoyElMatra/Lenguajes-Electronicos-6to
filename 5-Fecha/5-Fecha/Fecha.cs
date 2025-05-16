a31e6835b958a148c0a6171e87d4162c5c9a736b
SoyElMatra <matramatrayt@gmail.com>
2025-05-16 16:04:07 -0300

    5-Fecha NO ANDA

---
+++ 5-Fecha/5-Fecha/Fecha.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Fecha
+{
+    class Fecha
+    {
+        private int dia;
+        private int mes;
+        private int año;
+        private int[] diasPorMes = { 30, 27, 30, 29, 30, 29, 30, 30, 29, 30, 29, 30 };
+
+        public Fecha(int dia, int mes, int año)
+        {
+            Mes = mes;  // Primero, establecemos el mes
+            Dia = dia;  // Luego, establecemos el día
+            Año = año;
+        }
+
+        public int Dia
+        {
+            get { return dia; }
+            private set
+            {
+                int maxDias = diasPorMes[Mes];
+
+                // Si es febrero y es un año bisiesto, febrero tiene 29 días
+                if (Mes == 2 && EsBisiesto())
+                    maxDias = 29;
+
+                if (value >= 1 && value <= maxDias)
+                {
+                    dia = value;
+                }
+                else
+                {
+                    // Si el día es mayor al máximo, ajustamos al último día del mes
+                    if (value > maxDias)
+                    {
+                        dia = maxDias;
+                    }
+                    else
+                    {
+                        dia = 1;
+                    }
+                }
+            }
+        }
+
+        public int Mes
+        {
+            get { return mes; }
+            private set
+            {
+                if (value >= 1 && value <= 12)
+                {
+                    mes = value;
+                }
+                else
+                    mes = 1;
+            }
+        }
+
+        public int Año
+        {
+            get { return año; }
+            private set { año = value; }
+        }
+
+        public bool EsBisiesto()
+        {
+            return (año % 4 == 0 && año % 100 != 0) || (año % 400 == 0);
+        }
+
+        public override string ToString()
+        {
+            return $"{Mes.ToString("D2")}/{Dia.ToString("D2")}/{año}";
+        }
+    }
+}
---
+++ 5-Fecha/5-Fecha/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Fecha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo de uso
+            Fecha fecha1 = new Fecha(29, 2, 2024);  // Año bisiesto
+            Console.WriteLine(fecha1.ToString());  // Salida: 02/28/2024
+
+            Fecha fecha2 = new Fecha(29, 2, 2023);  // Año no bisiesto
+            Console.WriteLine(fecha2.ToString());  // Salida: 02/28/2023
+
+            Fecha fecha3 = new Fecha(32, 5, 2023);  // Abril tiene 30 días
+            Console.WriteLine(fecha3.ToString());  // Salida: 04/30/2023
+
+            Fecha fecha4 = new Fecha(31, 1, 2023);  // Enero tiene 31 días
+            Console.WriteLine(fecha4.ToString());  // Salida: 01/31/2023
+        }
+    }
+}
